cdfdf3d80ad4ca2d59728173190ef483a3007d54
fleurblanckaert <60987478+fleurblanckaert@users.noreply.github.com>
2020-09-30 11:27:38 -0400

    Changed Person.h, Person.cpp

--- OOP3200_InClassLesson4/Person.cpp
+++ OOP3200_InClassLesson4/Person.cpp
@@ -1,76 +1,100 @@
 /**
+ *Name: Fleur Blanckaert
  * Project OOP3200-F2020-Lesson4
  */
 
 
 #include "Person.h"
 
-/**
- * Person implementation
- */
+#include <iostream>
+
+ /**
+  * Person implementation
+  */
 
 
-/**
- * @param first_name
- * @param last_name
- * @param age
- */
-void Person::Person(string first_name, string last_name, float age) {
+  /**
+   * @param first_name
+   * @param last_name
+   * @param age
+   */
+Person::Person(std::string first_name, std::string last_name, const float age)
+    : m_age(age), m_firstName(std::move(first_name)), m_lastName(std::move(last_name))
+{
 
 }
 
 /**
  * @return float
  */
-float Person::getAge() {
-    return 0.0;
+float Person::getAge() const
+{
+    return m_age;
 }
 
 /**
  * @param value
  */
-void Person::setAge(float value) {
-
+void Person::setAge(const float value)
+{
+    m_age = value;
 }
 
 /**
- * @return string
+ * @return std::string
  */
-string Person::getFirstName() {
-    return "";
-}
-
-/**
- * @param value
- */
-void Person::setFirstName(string value) {
-
-}
-
-/**
- * @return string
- */
-string Person::getLastName() {
-    return "";
+std::string Person::getFirstName() const
+{
+    return m_firstName;
 }
 
 /**
  * @param value
  */
-void Person::setLastName(string value) {
+void Person::setFirstName(const std::string& value)
+{
+    m_firstName = value;
+}
 
+/**
+ * @return std::string
+ */
+std::string Person::getLastName() const
+{
+    return m_lastName;
+}
+
+/**
+ * @param value
+ */
+void Person::setLastName(const std::string& value)
+{
+    m_lastName = value;
 }
 
 /**
  * @return void
  */
-void Person::SaysHello() {
-    return;
+void Person::SaysHello() const
+{
+    std::cout << getFirstName() << " says Hello!" << std::endl;
 }
 
+
 /**
- * @return string
+ * @return std::string
  */
-string Person::ToString() {
-    return "";
+
+std::string Person::ToString()
+{
+    std::string output_string;
+
+    output_string += "---------------------------------------------\n";
+    output_string += "First Name: " + getFirstName() + "\n";
+    output_string += "Last Name : " + getLastName() + "\n";
+    output_string += "Age       : " + std::to_string(getAge()) + "\n";
+    output_string += "---------------------------------------------\n\n";
+
+    return output_string;
 }
+
--- OOP3200_InClassLesson4/Person.h
+++ OOP3200_InClassLesson4/Person.h
@@ -1,49 +1,54 @@
 /**
+ *Name:Fleur Blanckaert
  * Project OOP3200-F2020-Lesson4
  */
 
 
+#pragma once
 #ifndef _PERSON_H
 #define _PERSON_H
+#include <string>
 
 class Person {
-public: 
-    
-/**
- * @param first_name
- * @param last_name
- * @param age
- */
-void Person(string first_name, string last_name, float age);
-    
-float getAge();
-    
-/**
- * @param value
- */
-void setAge(float value);
-    
-string getFirstName();
-    
-/**
- * @param value
- */
-void setFirstName(string value);
-    
-string getLastName();
-    
-/**
- * @param value
- */
-void setLastName(string value);
-    
-void SaysHello();
-    
-string ToString();
-private: 
+public:
+
+    /**
+     * @param first_name
+     * @param last_name
+     * @param age
+     */
+
+    Person(std::string first_name, std::string last_name, float age);
+
+    float getAge() const;
+
+    /**
+     * @param value
+     */
+    void setAge(float value);
+
+    std::string getFirstName() const;
+
+    /**
+     * @param value
+     */
+    void setFirstName(const std::string& value);
+
+    std::string getLastName() const;
+
+    /**
+     * @param value
+     */
+    void setLastName(const std::string& value);
+
+    void SaysHello() const;
+
+    virtual std::string ToString();
+
+private:
     float m_age;
-    string m_firstName;
-    string m_lastName;
+    std::string m_firstName;
+    std::string m_lastName;
 };
 
 #endif //_PERSON_H
